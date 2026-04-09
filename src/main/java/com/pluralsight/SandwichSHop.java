9388a6c9d626343d8cc0329182d551ba28a12343
christian-nieves <thechristiannieves@gmail.com>
2026-04-09 12:38:26 -0400

    Asking question done

--- src/main/java/com/pluralsight/SandwichSHop.java
+++ src/main/java/com/pluralsight/SandwichSHop.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class SandwichSHop {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What size sandwich would you like?:");
+        System.out.println("1: Regular: $5.45");
+        System.out.println("2: Large: $8.95");
+
+        int sandwichSize = myScanner.nextInt();
+
+
+
+    }
 }
